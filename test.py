4b1a72bd3643903d154a0ca6d7c0dd60667407d6
wangxp01 <flywangle@hotmail.com>
2024-01-21 20:32:16 +0800

    新增测试

--- test.py
+++ test.py
@@ -8,13 +8,14 @@ def main():
     #### 登陆系统 ####
     lg = bs.login()
 
-    rs_list = []
-    rs_dividend = bs.query_dividend_data(code="sz.300888", year=2022, yearType="report")
-    while (rs_dividend.error_code == '0') & rs_dividend.next():
-        rs_list.append(rs_dividend.get_row_data())
-    print(rs_list)
-    dividOperateDate = rs_list[0][9]
-    print(dividOperateDate)
+    profit_list = []
+    rs_profit = bs.query_profit_data(code="sz.300888", year=2022, quarter=4)
+    while (rs_profit.error_code == '0') & rs_profit.next():
+        profit_list.append(rs_profit.get_row_data())
+    result_profit = pd.DataFrame(profit_list, columns=rs_profit.fields)
+    row_index = result_profit.index
+    print(row_index)
+    print(result_profit.loc[0,"totalShare"])
 
     # #### 结果集输出到csv文件 ####   
     # result_profit.to_csv("D:\\1\\history_Dividend_data.csv", encoding="gbk",index=False)
